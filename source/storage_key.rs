cf3f3eab5f65a1774dba529d4ac368edb1a28642
ZiminDmitriy <dimazimin96@yandex.ru>
2022-06-29 18:02:18 +0300

    XCC core/staking-pool II part

--- source/storage_key.rs
+++ source/storage_key.rs
@@ -1,7 +1,6 @@
 use near_sdk::borsh::{self, BorshSerialize};
 use near_sdk::BorshStorageKey;
 
-
 /// Do not change the order of variants.
 /// The number of options must be less than or equal to 256 (1 byte).
 #[derive(BorshSerialize, BorshStorageKey)]
